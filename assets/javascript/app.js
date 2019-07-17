6329f3941a1d37ff2fc1874b98bfc18f688e5620
BrianKohnen <briankohnen@yahoo.com>
2019-07-17 14:44:08 -0500

    bk -- added scripting, user can click buttons to generate 10 gifs on the topic. user can add their own buttons. clicking a gif on screen animates or causes it to be still depending on prior animation state.

---
+++ assets/javascript/app.js
@@ -0,0 +1,93 @@
+// giphy api key : x6ZEkt0MNBEzavwLgHLGLOyulDDrwcE6
+
+var topics = ["coding", "games", "movies", "coffee", "photos", "skateboard", "penguins", "dogs"];
+
+
+$(document).ready(function() {
+
+function buttonGenerate () {
+    $("#gifButtons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        $("#gifButtons").append($("<button class='gifGenButton'>" + topics[i] + "</button>"));
+    };
+};
+
+$(document).on("click", ".gifGenButton", function() {
+    
+    var queryTopic = $(this).text();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=x6ZEkt0MNBEzavwLgHLGLOyulDDrwcE6&q=" + queryTopic + "&limit=10&offset=0&rating=PG&lang=en";
+
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var results = response.data;
+        $("#allGifs").empty();
+
+        for (var i = 0; i < 10; i++) {
+
+            var stillUrl = results[i].images.original_still.url;
+            var animUrl = results[i].images.original.url;
+
+            var gifContainer = $("<div>");
+
+            var addGif = $("<img class='thisGif'>");
+            addGif.attr("state", "still");
+            addGif.attr("src", stillUrl);
+            addGif.attr("anim", animUrl);
+            addGif.attr("still", stillUrl);
+            addGif.attr("alt", queryTopic + " image");
+
+
+            var gifRating = $("<p>");
+            $(gifRating).text("Rating : " + results[i].rating);
+
+
+            $(gifContainer).append(addGif, gifRating);
+
+            $("#allGifs").prepend(gifContainer);
+            console.log(addGif.attr("class"));
+        }
+      });
+  });
+
+  $("#submitButton").on("click", function() {
+
+    event.preventDefault();
+
+    var userInput = $("#userInput").val().trim();
+
+    if (topics.includes(userInput.toLowerCase())) {
+        console.log("repeat topic");
+    }else if(userInput === "") {
+        console.log("invalid topic")
+    }else {
+    topics.push(userInput);
+    }
+
+    buttonGenerate();
+
+    });
+
+
+  $(document).on("click", ".thisGif", function() {
+
+      if ($(this).attr("state") == "still") {
+
+          $(this).attr("state", "animated");
+          
+          $(this).attr("src", $(this).attr("anim"));
+
+      } else {
+        
+        $(this).attr("state", "still");
+
+        $(this).attr("src", $(this).attr("still"));
+      }
+    });
+
+
+    buttonGenerate();
+});
